14acbe29d0fa93a4cdfab00fb9f62bb661604b1d
Joshua Storey & Jeremy Whitaker <student@epicodus.com>
2014-07-30 11:33:51 -0700

    create warehouse class variable and save method to store vehicles in the warehouse array

--- lib/car_store.rb
+++ lib/car_store.rb
@@ -1,13 +1,24 @@
 
 
+
+
 class Vehicle
+  @@warehouse = [] #class variable
   def initialize(make, model, year, price)
     @make = make
     @model = model
     @year = year
     @price = price
+    save
   end
 
+  def Vehicle.warehouse #calling the class method of warehouse
+    @@warehouse
+  end
+
+  def save #save method to shovel the current class instance into the warehouse array
+    @@warehouse << self
+  end
 
   def make
     @make
@@ -26,7 +37,13 @@ class Vehicle
   end
 end
 
-toyota_prius = Vehicle.new("Toyota", "Prius", "2014", "23999")
-chevy_camaro = Vehicle.new("Chevrolet", "Camaro", "2014", "45000")
+toyota_prius = Vehicle.new("Toyota", "Prius", "2014", 23999)
+chevy_camaro = Vehicle.new("Chevrolet", "Camaro", "2014", 45000)
+mini_cooper = Vehicle.new("Cooper", "Mini", "2013", 25000)
 
-p "You vehicle is a #{chevy_camaro.make} #{chevy_camaro.model}"
+p Vehicle.warehouse[0]
+# p "#{warehouse[0].make} #{warehouse[0].model}"
+# p "#{warehouse[1].make} #{warehouse[1].model}"
+
+
+# p "You vehicle is a #{chevy_camaro.make} #{chevy_camaro.model}"
--- spec/car_store_spec.rb
+++ spec/car_store_spec.rb
@@ -11,4 +11,9 @@ describe Vehicle do
     test_vehicle = Vehicle.new("Toyota","Prius","2014","23999")
     test_vehicle.make.should eq ("Toyota")
   end
+
+  it 'initializes a Warehouse class variable and make the vehicle object properties accessible' do
+    test_vehicle = Vehicle.new("Toyota","Prius","2014","23999")
+    Vehicle.warehouse[0].should be_an_instance_of Vehicle
+  end
 end
